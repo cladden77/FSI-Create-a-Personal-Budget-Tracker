f53ea89af9b7d634bfcd2cd8f446bc5e32cd92ac
Chris L <chris.ladden@gmail.com>
2022-02-17 18:56:39 -0600

    added script to display questions and answers

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-let weeklyExpenses = 0
-let monthlyExpenses = 0
-let annualExpenses = 0
+let weeklyExpenses = 0;
+let monthlyExpenses = 0;
+let annualExpenses = 0;
 
-// Your Code Here
+// Weekly Expenses
+for (let i = 0; i < weeklyExpenseQuestions.length; i++) {
+  let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]));
+  weeklyExpenses = weeklyExpenses + answer;
+}
+
+// Monthly Expenses
+for (let i = 0; i < monthlyExpenseQuestions.length; i++) {
+  let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]));
+  monthlyExpenses = monthlyExpenses + answer;
+}
+
+// Annual Expenses
+for (let i = 0; i < annualExpenseQuestions.length; i++) {
+  let answer = parseFloat(window.prompt(annualExpenseQuestions[i]));
+  annualExpenses = annualExpenses + answer;
+}
